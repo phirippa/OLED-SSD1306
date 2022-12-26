7063649b1f414273a25821c73f106e7724b1c6b1
phirippa <phirippa@naver.com>
2022-12-26 20:43:27 +0900

    Create OLED-ssd1306-setTextSize.ino

---
+++ OLED-ssd1306-setTextSize.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1  
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()   {                
+  if( !display.begin(SSD1306_SWITCHCAPVCC, 0x3C) ) { 
+    Serial.println(F("OLED(SSD1306) allocation failed"));
+    while(1) {}
+  }
+  display.clearDisplay();
+ 
+  display.setTextColor(WHITE);
+  display.setTextSize(1);   // 1 to 8
+  display.println("Hello World!");
+  display.println("Phi board v1.0");
+  display.display();
+}
+
+void loop() { }
